873cb60b8eb605007ad6bff4211509d7ae27efd9
Housey <housebreaker1@gmail.com>
2015-10-02 16:19:07 +0300

    remove fake dir structure + merge recursive and non-recursive solutions for problem 7

--- 06. Streams And Files/07. Directory Traversal/DirTraversal.cs
+++ 06. Streams And Files/07. Directory Traversal/DirTraversal.cs
@@ -1,37 +1,69 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 
-internal class DirectoryTraversal
+namespace _07.Directory_Traversal
 {
-	static void Main()
+	class DirectoryTraversal
 	{
-		string[] filePaths = Directory.GetFiles(@"../../");
+		static readonly string desktopPath = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
 
-		List<FileInfo> files = filePaths.Select(path => new FileInfo(path)).ToList();
+		const string reportFilename = "CSharp Streams and Files Homework Dir Report.txt";
+		static readonly string ReportPath = $"{desktopPath}\\{reportFilename}";
 
-		var sorted = files
-			.OrderBy(file => file.Length)
-			.GroupBy(file => file.Extension)
-			.OrderByDescending(group => group.Count())
-			.ThenBy(group => group.Key);
-
-		string desktopPath = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
-
-		using (var writer = new StreamWriter(desktopPath + "/report.txt"))
+		static void Main()
 		{
-			foreach (var group in sorted)
-			{
-				writer.WriteLine(group.Key);
+			const string startPath = @"..\..\";
 
-				foreach (var y in group)
+			if (File.Exists(ReportPath))
+				File.Delete(ReportPath); // making sure we don't just append to the same file if the program is run more than once
+
+			GenerateReport(startPath, true);
+
+			Console.WriteLine($"Done! Opening {ReportPath}.");
+
+			System.Diagnostics.Process.Start(ReportPath);
+		}
+
+		static void GenerateReport(string dir, bool recursive)
+		{
+			var filePaths = Directory.GetFiles(dir);
+
+			var files = filePaths.Select(path => new FileInfo(path)).ToList();
+
+			var sorted = files
+				.OrderBy(file => file.Length)
+				.GroupBy(file => file.Extension)
+				.OrderByDescending(group => @group.Count())
+				.ThenBy(group => @group.Key);
+
+			using (var writer = new StreamWriter(ReportPath, true))
+			{
+				if (Directory.GetFiles(dir).Count() != 0) // if the directory has no files to report, there's no use outputting anything
 				{
-					writer.WriteLine("--{0} - {1:F3}kb", y.Name, y.Length / 1024.0);
+					writer.WriteLine($"Current Dir: {dir}");
+
+					foreach (var group in sorted)
+					{
+						writer.WriteLine(@group.Key);
+
+						foreach (var y in @group)
+						{
+							writer.WriteLine("--{0} - {1:F3}kb", y.Name, y.Length / 1024.0);
+						}
+					}
+					writer.WriteLine();
+				}
+			}
+
+			if (recursive)
+			{
+				var subDirs = Directory.GetDirectories(dir);
+				foreach (var subDir in subDirs)
+				{
+					GenerateReport(subDir, true);
 				}
 			}
 		}
-
-		System.Diagnostics.Process.Start(desktopPath + "/report.txt");
 	}
 }
